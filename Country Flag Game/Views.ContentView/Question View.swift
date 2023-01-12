7717dc6cb22f9711bbf6bff366cb8b6c6ffd986d
Faiz Ali <fali@bsd220.org>
2023-01-11 19:52:59 -0600

    Modified QuestionView

--- Country Flag Game/Views.ContentView/Question View.swift
+++ Country Flag Game/Views.ContentView/Question View.swift
@@ -6,9 +6,10 @@
 //
 
 import SwiftUI
-
 struct Question_View: View {
+    @EnvironmentObject var quizManager: QuizManager
     var body: some View {
+        if quizManager.playingGame {
         VStack(spacing: 20) {
             HStack {
                 Text("Country Flag Quiz")
@@ -16,28 +17,54 @@ struct Question_View: View {
                     .font(.title)
                     .fontWeight(.heavy)
                 Spacer()
-                Text("1 out of 3")
+                Text("\(quizManager.index) out of \(quizManager.questions.count)")
                     .foregroundColor(.yellow)
                     .fontWeight(.heavy)
             }
-            Progress_Bar(progress: 50)
+            Progress_Bar(progress: quizManager.progress)
             VStack(spacing: 20) {
                 Text("Which country's flag is this?")
                     .font(.title)
-                Image("Italy")
+                Image(quizManager.country)
                     .resizable()
                     .frame(width: 300, height: 200)
-                AnswerRow(answer: Answer(text: "France", isCorrect: false))
-                AnswerRow(answer: Answer(text: "Germany", isCorrect: false))
-                AnswerRow(answer: Answer(text: "Italy", isCorrect: true))
-                AnswerRow(answer: Answer(text: "England", isCorrect: false))
+                ForEach(quizManager.answerChoices) {
+                    answer in AnswerRow(answer: answer)
+                        .environmentObject(quizManager)
+                }
             }
-            Custom_Button(text: "Next")
+            
+            Button {
+                quizManager.goToNextQuestion()
+            } label: {
+                Custom_Button(text: "Next", background: quizManager.answerSelected ? .yellow : .gray)
+            }
+            .disabled(!quizManager.answerSelected)
             Spacer()
             .padding()
             .frame(width: .infinity, height: .infinity)
             .background(.cyan)
         }
+        } else {
+            VStack(spacing: 20) {
+                Text("Country Flag Quiz")
+                    .font(.title)
+                Text("Congratulations! You have completed the quiz.")
+                Text("You Scored\(quizManager.score) out of \(quizManager.questions.count)")
+                Button {
+                    quizManager.reset()
+                    
+                } label: {
+                    Custom_Button(text: "Play Again")
+                }
+                }
+            .foregroundColor(.yellow)
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.cyan)
+            }
+        }
+        
     }
     
     struct Question_View_Previews: PreviewProvider {
@@ -45,4 +72,4 @@ struct Question_View: View {
             Question_View()
         }
     }
-}
+

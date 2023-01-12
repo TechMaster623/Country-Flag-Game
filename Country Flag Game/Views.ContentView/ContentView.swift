f39be1e14c62092fadffbda44a8d34b7b0902539
Faiz Ali <fali@bsd220.org>
2023-01-11 19:03:04 -0600

    created initial View

--- Country Flag Game/Views.ContentView/ContentView.swift
+++ Country Flag Game/Views.ContentView/ContentView.swift
@@ -8,19 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var quizManager = QuizManager()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 40) {
+                VStack(spacing: 20) {
+                    Text("Country Flag Game")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.yellow)
+                    Text("Ready to test your skills?")
+                        .foregroundColor(.yellow)
+                }
+                NavigationLink {
+                Question_View()
+                        .environmentObject(quizManager)
+                    
+                } label: {
+                    Custom_Button(text: "Start")
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea(.all)
+            .background(.cyan)
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
